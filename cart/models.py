cb2166e1a8cd22ffef331c2eb202af355e89d2af
gamechanger638 <rv795704@gmail.com>
2025-02-08 12:12:12 +0530

    i have added desings

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+ 
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Product Name")
+    description = models.TextField(verbose_name="Product Description", null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Price")
+    image1 = models.ImageField(upload_to='products/', verbose_name="Image 1", blank=True, null=True)
+    image2 = models.ImageField(upload_to='products/', verbose_name="Image 2", blank=True, null=True)
+    image3 = models.ImageField(upload_to='products/', verbose_name="Image 3", blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect ,get_object_or_404
 
 # Create your views here.
+from .models import Product
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'cart/product_list.html', {'products': products})
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    return render(request, 'cart/product_detail.html', {'product': product})
